4932f96c373ba46f6ddc5ceb273b9703fcbe7252
Benoît Müller <82948763+Benoit-Muller@users.noreply.github.com>
2024-04-13 18:30:37 +0200

    cvx function solver works

--- data.m
+++ data.m
@@ -1,4 +1,5 @@
 function A = data(n,k,r)
     Y = randn(n,r,k);
     A = multiprod(Y,multitransp(Y));
+    A= 0.5*(A+multitransp(A));
 end
--- multisqrtm.m
+++ multisqrtm.m
@@ -2,6 +2,11 @@ function X = multisqrtm(A)
     [n,m,k] = size(A);
     X = zeros(n,m,k);
     for j=1:k
-        X(:,:,j) = sqrt(A(:,:,j));
+        [V,D] = eig(A(:,:,j));
+        V=real(V);
+        D=real(D);
+        D(D<0)=0;
+        X(:,:,j) = V*sqrt(D)*V';
+        X(:,:,j)= 0.5*(X(:,:,j)+multitransp(X(:,:,j)));
     end
 end
