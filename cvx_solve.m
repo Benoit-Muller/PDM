fbdbc7fd434a45a6f47e52723d4168df3d701284
Benoît Müller <82948763+Benoit-Muller@users.noreply.github.com>
2024-04-17 18:55:36 +0200

    new graphics

--- cvx_solve.m
+++ cvx_solve.m
@@ -10,15 +10,15 @@ trA = sum(multitrace(A));
 
 cvx_begin sdp quiet
     variable B( n, n ) symmetric
-    dual variable D
     minimize(trace(B) + trA/k - 2/k*trace_sqrtm(diagAsqrt * kron(eye(k),B) * diagAsqrt))
-    B >= 0 : D;
+    B >= 0
 cvx_end
 
 Bcost=cvx_optval;
-info.cvx_status=cvx_status;
-info.cvx_slvitr=cvx_slvitr;
-info.cvx_slvtol=cvx_slvtol;
-info.cvx_optbnd=cvx_optbnd;
-info.cvx_cputime=cvx_cputime;
+info.cost = cvx_optval;
+info.status = cvx_status;
+info.iter = cvx_slvitr;
+info.tol = cvx_slvtol;
+info.optbnd = cvx_optbnd;
+info.time = cvx_cputime;
 end
