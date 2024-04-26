e0712b15ca41a806c849aacdd9a5798722c349e8
Benoît Müller <82948763+Benoit-Muller@users.noreply.github.com>
2024-04-26 17:14:35 +0200

    data_scaled works

--- data_scaled.m
+++ data_scaled.m
@@ -1,9 +1,15 @@
-function A = data_scaled(B,k)
-% Generate data samples that follow a distribution whose BW barycenter is
-% (suposedly) B.
+function A = data_scaled(B,k,version)
+% Generate data samples that follow a distribution whose BW barycenter is B
+% If version="exact", the empirical barycenter of data is also exactly B.
     n = size(B,1);
     Z = randn(n,n,k);
-    ZZt = pagemtimes(Z,"none",Z,"transpose");
-    A = 1/n^2 * pagemtimes(pagemtimes(ZZt,B),ZZt);
+    C = 1/n * pagemtimes(Z,"none",Z,"transpose"); % Whishart
+    % could be any distribution on psd matrices and with expectation I.
+    if nargin==3 && version=="exact"
+        [V,D]=eig(mean(C,3));
+        correction = V*D^(-0.5)*V';
+        C = pagemtimes(pagemtimes(correction,C),correction);
+    end
+    A =  pagemtimes(pagemtimes(C,B),C);
     A = multisym(A); % numeric purpose
 end
