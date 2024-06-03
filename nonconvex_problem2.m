96d3f3cf553d807eb9bd3b66e72a71a84292d406
Benoît Müller <82948763+Benoit-Muller@users.noreply.github.com>
2024-06-03 16:05:29 +0200

    nonconvex_problem2 works

--- nonconvex_problem2.m
+++ nonconvex_problem2.m
@@ -14,24 +14,33 @@ function problem = nonconvex_problem2(A,r,p)
         [V,D] = eigs(A(:,:,i),r); %eigs
         problem.Y3(:,:,i) = V*sqrt(D);
     end
-    problem.Y2 = problem.M.to2D(Y3);
+    problem.Yto2D = @Yto2D;
+    function Y2 = Yto2D(Y3)
+        [dim1,dim2,dim3] = size(Y3);
+        Y2 = reshape(Y3, [n, k*r]);
+    end
+    problem.Y2 = problem.Yto2D(problem.Y3);
 
-    problem.chi = @(Q) 1/k * Q*problem.Y2;
-    normY = norm(Y2,"fro");
-    problem.cost = @(Q) -1/k^2 * norm(problem.Y2*Q,"fro") + 1/k*normY;
+    problem.chi = @(Q) 1/k * problem.Y2*Q;
+    normY = norm(problem.Y2,"fro");
+    problem.cost = @(Q) -1/k^2 * norm(problem.Y2*Q,"fro")^2 + 1/k*normY^2;
 
     problem.egrad = @(Q) -2/k^2 * problem.Y2' * (problem.Y2 * Q);
 
     problem.costgrad = @costgrad;
     function [c,g] = costgrad(Q)
         Y2Q = problem.Y2*Q;
-        c = -1/k^2 * norm(Y2Q,"fro") + 1/k*normY;
+        c = -1/k^2 * norm(Y2Q,"fro")^2 + 1/k*normY^2;
         g = -2/k^2 * problem.Y2' * Y2Q;
-        g = M.egrad2rgrad(g);
+        g = problem.M.egrad2rgrad(Q,g);
     end
 
-    problem.ehess = @(Q,v) problem.egrad(V);
-    problem.B =@()
+    problem.ehess = @(Q,V) problem.egrad(V);
+    problem.Q2B = @Q2B;
+    function B=Q2B(Q)
+        X=problem.Y2 * Q;
+        B=X*X';
+    end
     problem.variance = @(B) cost_variance(problem.A,B);
     problem.egrad_variance = @(B) egrad_variance(problem.A,B);
 end
--- temp.m
+++ temp.m
@@ -1,9 +1,9 @@
 clear
-n=5; % data dimension
-k=5; % number of matrices
+n=4; % data dimension
+k=3; % number of matrices
 r=round(n/2); % size of factorization Y of data A
 p=r+1; % size of factorization X of barycenter B (r<=p)
 A = data(n,k,r);
-prob = nonconvex_problem(A,r,p);
+prob = nonconvex_problem2(A,r,p);
+
 checkgradient(prob);
-checkhessian(prob);
